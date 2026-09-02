70af78b72593c7464c648a369595466922f994de
Abinash Behera <166229807+AbinashB017@users.noreply.github.com>
2026-09-02 15:13:27 +0530

    Time: 15 ms (9.97%) | Memory: 20.3 MB (16.90%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(n);
+        vector<int>indegree(n,0);
+        for(auto it:prerequisites){
+            int course=it[0];
+            int prereq=it[1];
+            adj[prereq].push_back(course);
+            indegree[course]++;
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(topo.size()==n)return true;
+        return false;
+    }
+};
