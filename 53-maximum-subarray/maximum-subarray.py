d30360084363f03ce6b102fdc38eaf213f1afdc9
Abinash Behera <166229807+AbinashB017@users.noreply.github.com>
2026-09-06 15:58:34 +0530

    Time: 32 ms (68.50%) | Memory: 31.4 MB (74.24%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,8 +4,8 @@ class Solution:
         sum=0
         for i in range(len(nums)):
             sum+=nums[i]
-            if(sum>maxi):
-                maxi=sum
+            maxi=max(maxi,sum)
+
             if sum<0:
                 sum=0
-        return maxi            
+        return maxi;        
