21aa5580252aaa51bb5cf63f3ece20abdb609aed
Abinash Behera <166229807+AbinashB017@users.noreply.github.com>
2026-09-06 23:14:31 +0530

    Time: 10 ms (49.84%) | Memory: 19.2 MB (99.57%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -3,16 +3,17 @@ class Solution:
         if index==len(nums):
             if target==0:
                 ans.append(temp.copy())
-                return
         if index==len(nums):
-            return 
+            return
         if nums[index]<=target:
             temp.append(nums[index])
             self.solve(index,target-nums[index],nums,ans,temp)
             temp.pop()
         self.solve(index+1,target,nums,ans,temp)                
+
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        temp=[]
         ans=[]
+        temp=[]
         self.solve(0,target,candidates,ans,temp)
         return ans
+        
